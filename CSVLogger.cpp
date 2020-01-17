c92542e55c216204c1342f975491a59beccd7b68
Abhishek Kulkarni <abhishek7kulkarni@gmail.com>
2020-01-17 10:49:56 +0000

    Implementing Timestamped filename

--- CSVLogger.cpp
+++ CSVLogger.cpp
@@ -4,12 +4,23 @@
  *  Created on: 7 Jan 2020
  *      Author: abhishekkulkarni
  */
-
+#include <ctime>
+#include <string.h>
 #include "CSVLogger.h"
 
 CSVLogger::CSVLogger(const char* filename)
 {
-	csvfile_.open(filename, std::ios::out | std::ios::app);
+	time_t ttime = time(0);
+	tm *local_time = localtime(&ttime);
+
+	char datetime[20];
+	strftime(datetime, 20, "_%Y%m%d%H%M%S.csv", local_time);
+
+	char filename_timestamp[100];
+	strcpy(filename_timestamp,filename);
+	strcat(filename_timestamp,datetime);
+
+	csvfile_.open(filename_timestamp, std::ios::out | std::ios::app);
 }
 
 CSVLogger::~CSVLogger()
